37e6144ad1ae8e4b6afc7f81d68b796d503b045e
JOON LEE <110060709+greenlemonkp@users.noreply.github.com>
2023-04-10 19:22:52 -0400

    Add components

--- my-next-project/src/components/Card1.js
+++ my-next-project/src/components/Card1.js
@@ -8,7 +8,6 @@ import CardActions from "@mui/material/CardActions";
 import Collapse from "@mui/material/Collapse";
 import IconButton from "@mui/material/IconButton";
 import Typography from "@mui/material/Typography";
-import { red } from "@mui/material/colors";
 import FavoriteIcon from "@mui/icons-material/Favorite";
 import ShareIcon from "@mui/icons-material/Share";
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
--- my-next-project/tailwind.config.js
+++ my-next-project/tailwind.config.js
@@ -2,12 +2,7 @@
 module.exports = {
   content: ["./src/**/*.{html,js,jsx,tsx}"],
   theme: {
-    extend: {
-      colors: {   
-        violet: {
-          100: '#a78bfa'
-      }
-    },
+    extend: {},
   },
   plugins: [],
 };
