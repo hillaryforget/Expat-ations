0fde5b8e6512022432197b4302d0db352d6bdcbd
Cvanimschoot <clayton.vanimschoot@gmail.com>
2023-04-12 03:29:03 -0600

    Add piegraph and piegraphcard component. Fill with temp data.

--- my-next-project/src/components/Card.js
+++ my-next-project/src/components/Card.js
@@ -1,19 +1,19 @@
 import * as React from 'react';
 
-export default function MediaCard(props) {
+export default function Card(props) {
   const [name, image, summary] = [props.name, props.image, props.summary]
   console.log("+", image);
-  console.log("++", summary);
+  // console.log("++", summary);
   
   return (
-    <div className="rounded-xl shadow-lg bg-gray-100">
-      <div className="flex flex-col">
+     <div className="rounded-xl shadow-lg bg-gray-100 min-h-full max-w-screen-lg">
+       <div className="flex flex-col">
         <div className="rounded-t-xl overflow-hidden">
-          <img src={image} alt="" />
+           <img src={image} alt="" />
         </div>
         <div className="px-3">
           <h5 className="text-2xl md:text-3xl font-medium mt-3">{name}</h5>
-          <p className="text-lg mt-3">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam dui risus, molestie vitae ullamcorper ac, fermentum id sem. Integer iaculis dapibus ex a fringilla. Sed posuere eleifend mauris, nec tempor ex ornare in. Integer pellentesque ullamcorper felis, maximus pharetra nulla blandit sit amet. Quisque tempor nec velit sed pulvinar. Phasellus volutpat purus vitae leo ullamcorper, et volutpat quam malesuada. Cras suscipit nunc ut odio lobortis semper.</p>
+          <p className="text-lg mt-3" dangerouslySetInnerHTML={{__html: summary}}></p>
         </div>
       </div>
     </div>
--- my-next-project/src/pages/main.jsx
+++ my-next-project/src/pages/main.jsx
@@ -7,37 +7,43 @@ import SearchField from "../components/SearchField";
 import { useRouter } from 'next/router' 
 import { useEffect, useState } from "react";
 import Loading from "../components/Loading";
+import PieGraphCard from "../components/PieGraphCard";
 
 export async function getServerSideProps(context) {
   const query_one = context.query.city1.toLowerCase();
   const query_two = context.query.city2.toLowerCase();
 
-  // BASE LEVEL OF INFORMATION (name, continent,mayor, etc) //
+  //#region BASE LEVEL OF INFORMATION (name, continent,mayor, etc) //
   const res_city_one = await fetch(`https://api.teleport.org/api/urban_areas/slug:${query_one}/`);
   const data_city_one = await res_city_one.json();
 
   const res_city_two = await fetch(`https://api.teleport.org/api/urban_areas/slug:${query_two}/`);
   const data_city_two = await res_city_two.json();
+  //#endregion
 
-  // IMAGE INFORMATION (web-link, mobile-link, attributions) //
+  //#region IMAGE INFORMATION (web-link, mobile-link, attributions, etc) //
   const res_city_one_img = await fetch(`https://api.teleport.org/api/urban_areas/slug:${query_one}/images`);
   const data_city_one_img = await res_city_one_img.json();
 
   const res_city_two_img = await fetch(`https://api.teleport.org/api/urban_areas/slug:${query_two}/images`);
   const data_city_two_img = await res_city_two_img.json();
+  //#endregion
 
-  // SCORE INFORMATION (Housing score, COL score, summary) //
+  //#region SCORE INFORMATION (Housing score, COL score, summary, etc) //
   const res_city_one_score = await fetch(`https://api.teleport.org/api/urban_areas/slug:${query_one}/scores/`);
   const data_city_one_score = await res_city_one_score.json();
 
   const res_city_two_score = await fetch(`https://api.teleport.org/api/urban_areas/slug:${query_two}/scores`);
   const data_city_two_score = await res_city_two_score.json();
+  //#endregion
 
-  // const res_city_one_desc = await fetch(`https://api.teleport.org/api/urban_areas/slug:${query_one}/details`);
-  // const data_city_one_desc = await res_city_one.json();
+  //#region DETAILS INFORMATION (Weather, crime rate, cost of products, etc) //
+  const res_city_one_det = await fetch(`https://api.teleport.org/api/urban_areas/slug:${query_one}/details`);
+  const data_city_one_det = await res_city_one_det.json();
 
-  // const res_city_two_desc = await fetch(`https://api.teleport.org/api/urban_areas/slug:${query_two}/details`);
-  // const data_city_two_desc = await res_city_two.json();
+  const res_city_two_det = await fetch(`https://api.teleport.org/api/urban_areas/slug:${query_two}/details`);
+  const data_city_two_det = await res_city_two_det.json();
+  //#endregion
   
   return {
     props: {
@@ -46,7 +52,9 @@ export async function getServerSideProps(context) {
       city_one_img: data_city_one_img,
       city_two_img: data_city_two_img,
       city_one_score: data_city_one_score,
-      city_two_score: data_city_two_score
+      city_two_score: data_city_two_score,
+      city_one_det: data_city_one_det,
+      city_two_det: data_city_two_det
     }, // will be passed to the page component as props
   }
 }
@@ -58,18 +66,23 @@ export default function Main(props) {
   const router = useRouter()
   const query = router.query
 
-  console.log("City One", props.city_one);
-  console.log("City Two", props.city_two);
-  console.log("City One Image", props.city_one_img);
-  console.log("City Two Image", props.city_two_img);
-  console.log("City One Score", props.city_one_score);
-  console.log("City Two Score", props.city_two_score);
+  //#region Console Logs
+  // console.log("City One", props.city_one);
+  // console.log("City Two", props.city_two);
+  // console.log("City One Image", props.city_one_img);
+  // console.log("City Two Image", props.city_two_img);
+  // console.log("City One Score", props.city_one_score);
+  // console.log("City Two Score", props.city_two_score);
+  // console.log("City One Details", props.city_one_det);
+  // const data_obj = props.city_one_det.categories[8].data;
+  // console.log("Data obj", data_obj);
+  // console.log("Data obj values", Object.values(data_obj)[0]);
+  // console.log("City Two Details", props.city_two_det);
+  //#endregion
 
   useEffect(()=>{
 
   },[])
-
-
 
   return (
     <>
@@ -87,14 +100,32 @@ export default function Main(props) {
       {/* <div className="flex justify-center">
         <SearchField />
       </div> */}
-      <div className="flex flex-row gap-4 justify-center">
-        <div className="p-4">
-          <Card name={props.city_one.full_name} image={props.city_one_img.photos[0].image.web} summary={props.city_one_score.summary}/>
+        <div className="flex flex-row gap-4 justify-center">
+          <div className="p-4">
+            <Card name={props.city_one.full_name} image={props.city_one_img.photos[0].image.web} summary={props.city_one_score.summary}/>
+          </div>
+          <div className="p-4">
+            <Card name={props.city_two.full_name} image={props.city_two_img.photos[0].image.web} summary={props.city_two_score.summary}/>
+          </div>
         </div>
-        <div className="p-4">
-          <Card name={props.city_two.full_name} image={props.city_two_img.photos[0].image.web} summary={props.city_two_score.summary}/>
+        <div className="flex flex-row gap-4 justify-around">
+          <div className="flex flex-row gap-4">
+            <div className="p-4">
+              <PieGraphCard name={props.city_one_det.categories[8].label} data={props.city_one_det.categories[8].data} background_colors={['aqua', 'red', 'grey', 'yellow']} />
+            </div>
+            <div className="p-4">
+              <PieGraphCard name={props.city_one_det.categories[10].label} data={props.city_one_det.categories[10].data} background_colors={['aqua', 'red', 'grey', 'yellow']} />
+            </div>
+          </div>
+          <div className="flex flex-row gap-4">
+            <div className="p-4">
+              <PieGraphCard name={props.city_two_det.categories[8].label} data={props.city_two_det.categories[8].data} background_colors={['green', 'orange', 'pink', 'maroon']} />
+            </div>
+            <div className="p-4">
+            <PieGraphCard name={props.city_two_det.categories[9].label} data={props.city_two_det.categories[9].data} background_colors={['green', 'orange', 'pink', 'maroon']} />
+            </div>
+          </div>
         </div>
-      </div>
       </div> } 
     </>
   );
