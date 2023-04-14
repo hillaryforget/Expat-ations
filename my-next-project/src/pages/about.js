051f04b0f927bd1185dad43a064a948069ac006a
Hillary Forget <hillaryforget@outlook.com>
2023-04-14 12:31:49 -0400

    contact page

--- my-next-project/src/pages/about.js
+++ my-next-project/src/pages/about.js
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import Script from "next/script";
 import NavBar from "../components/NavBar";
 
 const About = () => {
---
+++ my-next-project/src/pages/contact.js
@@ -0,0 +1,70 @@
+import { useState } from 'react';
+import Navbar from '../components/Navbar';
+import Head from 'next/head';
+
+const Contact = () => {
+  const [email, setEmail] = useState('');
+  const [message, setMessage] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Handle form submission logic here, e.g., sending an email
+
+    // Reset the form fields
+    setEmail('');
+    setMessage('');
+  };
+
+  return (
+    <>
+      <Head>
+        <title>Contact - Expat-ations</title>
+        <meta name="description" content="About Expat-ations" />
+        <link rel="icon" href="/plane-solid.svg" />
+        <script src="https://cdn.tailwindcss.com"></script>
+      </Head>
+      <Navbar /> {/* Add the Navbar component */}
+      <div className="max-w-md mx-auto mt-10">
+        <h1 className="text-4xl font-bold mb-8">Contact Us</h1>
+        <form onSubmit={handleSubmit} className="space-y-4">
+          <div>
+            <label htmlFor="email" className="block text-sm font-medium text-gray-700">
+              Email
+            </label>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              className="mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+            />
+          </div>
+          <div>
+            <label htmlFor="message" className="block text-sm font-medium text-gray-700">
+              Your Message
+            </label>
+            <textarea
+              id="message"
+              name="message"
+              rows="4"
+              value={message}
+              onChange={(e) => setMessage(e.target.value)}
+              className="mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+            ></textarea>
+          </div>
+          <div>
+            <button
+              type="submit"
+              className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-blue-500 hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+            >
+              Submit
+            </button>
+          </div>
+        </form>
+      </div>
+    </>
+  );
+};
+
+export default Contact;
