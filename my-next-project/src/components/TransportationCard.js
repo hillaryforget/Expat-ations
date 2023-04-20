f131817bb921ff28ae6747d2547287822f2b5a8a
sweetmangoes <alphonsechristian@gmail.com>
2023-04-19 20:51:43 -0400

    created template to display info

--- my-next-project/src/components/TransportationCard.js
+++ my-next-project/src/components/TransportationCard.js
@@ -4,6 +4,12 @@ export default function TransportationCard({cityName}){
       <div className=" flex justify-center text-4xl">
         <h1>{cityName} Transportation</h1>
       </div>
+      <div className=" p-8 ">
+      <img src='transportation.svg' />
+      </div>
+      <div className="flex justify-center">
+      <p className="text-2xl"> Monthly Cost: <b>$85</b></p>
+      </div>
     </div>
   );
 }
